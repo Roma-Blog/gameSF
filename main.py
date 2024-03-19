ae04b2e6943aa82181c9c16a316902c68ea3e1a5
Roman <ryzikov14@gmail.com>
2024-03-19 16:50:23 +0700

    final

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ playing_field = [
 my_char = None
 ai_char = ''
 
-step = 0
+game = True
 
 def Clear_Console():
     if platform == "linux" or platform == "linux2":
@@ -37,7 +37,7 @@ def Start_Game():
 def Input_coordinates():
     global my_char
     print("-------------------------------------------")
-    coordinates = input(f"Напишите координаты (два числа столбец|строка) куда поставить {my_char}: ")
+    coordinates = input(f"Напишите координаты (два числа строка|столбец) куда поставить {my_char}: ")
 
     try:
         chek_num = all(int(i) < 3 and int(i) > -1 for i in coordinates)
@@ -54,12 +54,27 @@ def Input_coordinates():
             Input_coordinates()
             return
     else:
-        print("Коррдинаты нужно писать так, дава числа: 01 (столбец|строка)")
+        print("Коррдинаты нужно писать так, дава числа: 01 (строка|столбец)")
         Input_coordinates()
 
 def Win(char):
-    if Check_Row(char) or Check_Columns(char) or Check_Diagonal(char, 1) or Check_Diagonal(char, -1):
+    if Check_Row(char, 3) or Check_Columns(char, 3) or Check_Diagonal(char, 1, 3) or Check_Diagonal(char, -1, 3):
         return True
+
+def Stop_Game(message):
+    global game
+    game = False
+    Clear_Console()
+    Draw_playing_field(playing_field)
+    print("-----------------------------------------")
+    print(message)
+    print("-----------------------------------------")
+
+def Check_Drawn_Game():
+    dash_char = 0
+    for item in playing_field:
+        dash_char += item.count('-')
+    if dash_char <= 1: return True
 
 ###################################### AI ###########################################################
 
@@ -90,80 +105,93 @@ def Insert_Char_Diagonal(dir):
         num_row += 1
         num_col += 1 * dir
 
-def Win_Strategy(): 
-    if Check_Row(ai_char) or Check_Columns(ai_char) or Check_Diagonal(ai_char, 1) or Check_Diagonal(ai_char, -1):
-        return True
+def Strategy(char): 
+    check_row = Check_Row(char, 2, 1)
+    check_col = Check_Columns(char, 2, 1)
+    check_diag = Check_Diagonal(char, 1, 2, 1) or Check_Diagonal(char, -1, 2, 1)
 
-def Protect_Strategy():
-    if Check_Row(my_char) or Check_Columns(my_char) or Check_Diagonal(my_char, 1) or Check_Diagonal(my_char, -1):
+    if check_row != None:
+        Insert_Char_Row(check_row)
         return True
+    elif check_col != None:
+        Insert_Char_Col(check_col)
+        return True
+    elif check_diag != None:
+        Insert_Char_Diagonal(check_diag)
+        return True
+    return False
 
-def Check_Row(char):
+def Check_Row(char, count, dash_char = 0):
     num_row = 0
+    
     while num_row < 3:
-        if playing_field[num_row].count(char) == 3:
-            return 'Win'
-        elif playing_field[num_row].count(char) == 2 and playing_field[num_row].count('-') == 1:
-            Insert_Char_Row(num_row)
-            return True
+        if playing_field[num_row].count(char) == count and playing_field[num_row].count('-') == dash_char:
+            return num_row
         num_row += 1
 
-def Check_Columns(char):
+
+def Check_Columns(char, count, dash_char = 0):
     num_col = 0
 
     while num_col < 3:
-        count_char, dash_char, num_row= 0, 0, 0
+        count_char, dch, num_row= 0, 0, 0
 
         while num_row < 3:
             if playing_field[num_row][num_col] == char:
                 count_char += 1
             elif playing_field[num_row][num_col] == '-':
-                dash_char += 1
+                dch += 1
             num_row += 1
 
-        if count_char == 3:
-            return 'Win'
-        elif count_char == 2 and dash_char == 1:
-            Insert_Char_Col(num_col)
-            return True
+        if count_char == count and dch == dash_char:
+            return num_col
         num_col += 1
 
-def Check_Diagonal(char, dir):
+def Check_Diagonal(char, dir, count, dash_char = 0):
     num_col = -1 if dir < 0 else 0
-    num_row, dash_char, count_char = 0, 0, 0
+    num_row, dch, count_char = 0, 0, 0
     
     while num_row < 3:
         if playing_field[num_row][num_col] == char:
             count_char += 1
         elif playing_field[num_row][num_col] == '-':
-            dash_char += 1
+            dch += 1
         num_row += 1
         num_col += 1 * dir
     
-    if count_char == 3:
-        return 'Win'
-    elif count_char == 2 and dash_char == 1:
-        Insert_Char_Diagonal(dir)
-        return True
+    if count_char == count and dch == dash_char:
+        return dir
 
 def AI_step():
-    Win_Strategy() or Protect_Strategy() or Random_Step(random.randrange(0, 3),random.randrange(0, 3))
+    Strategy(ai_char) or Strategy(my_char) or Random_Step(random.randrange(0, 3),random.randrange(0, 3))
     Draw_playing_field(playing_field)
-
-
-
 
 Clear_Console()
 Start_Game()
 Draw_playing_field(playing_field)
 
-while True:
+while game:
     if my_char == "0":
         Input_coordinates()
-        if Win(my_char):
-            print("Вы победили!")
+        if Check_Drawn_Game():
+            Stop_Game("Ничья!!!")
+            break
+        elif Win(my_char):
+            Stop_Game("Вы победили!!!")
+            break
         AI_step()
+        if Win(ai_char):
+            Stop_Game("Компьютер победил!!!")
+            break
     else:
         AI_step()
+        if Check_Drawn_Game():
+            Stop_Game("Ничья!!!")
+            break
+        elif Win(ai_char):
+            Stop_Game("Компьютер победил!!!")
+            break
         Input_coordinates()
-
+        if Win(my_char):
+            Stop_Game("Вы победили!!!")
+            break
